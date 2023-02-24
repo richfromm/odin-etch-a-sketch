ff79836c18ea75c8a1c7cd6521b7041209251571
Rich Fromm <github@richard.fromm.name>
2023-02-23 17:39:27 -0800

    switch elements back to constants
    I had previously changed uses of `const` to `let` trying to debug some
    other problem. That's now solved, and this wasn't the underlying
    cause, so revert back.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -8,7 +8,7 @@ const SQUARE_SIZE = 32
 /* Initialize the container */
 function initContainer(squaresPerEdge, squareSizePixels, squareColor) {
     console.log("Initializing container")
-    let container = document.querySelector('#container');
+    const container = document.querySelector('#container');
     for (let x = 0; x < squaresPerEdge; x++) {
         // console.log(`Adding row ${x}`);
         addRow(container, squaresPerEdge, squareSizePixels, squareColor);
@@ -19,7 +19,7 @@ function initContainer(squaresPerEdge, squareSizePixels, squareColor) {
 
 /* Create a new row and add it to the container */
 function addRow(container, squaresPerEdge, squareSizePixels, squareColor) {
-    let row = document.createElement('div');
+    const row = document.createElement('div');
     row.classList.add('row')
     for (let y = 0; y < squaresPerEdge; y++) {
         // console.log(`Adding square ${y}`);
@@ -30,7 +30,7 @@ function addRow(container, squaresPerEdge, squareSizePixels, squareColor) {
 
 /* Create a new square and add it to the row */
 function addSquare(row, squareSizePixels, squareColor) {
-    let square = document.createElement('div');
+    const square = document.createElement('div');
     square.classList.add('square');
     square.style.width = `${squareSizePixels}px`;
     square.style.height = `${squareSizePixels}px`;
